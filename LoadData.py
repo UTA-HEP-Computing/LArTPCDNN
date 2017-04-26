1b7133726e3bd83da751151f680a5fb99c3fec0f
Amir Farbin <afarbin@gmail.com>
2017-04-26 17:28:11 -0500

    New Loader

--- LoadData.py
+++ LoadData.py
@@ -2,12 +2,9 @@ import h5py
 import glob,os,sys,time
 import numpy as np
 
-from DLTools.ThreadedGeneratorTest import DLMultiClassGenerator
-from DLTools.ThreadedGeneratorTest import DLh5FileGenerator
+from DLTools.ThreadedGenerator import DLMultiClassFilterGenerator
 
 #[u'Eng', u'Track_length', u'enu_truth', u'features', u'lep_mom_truth', u'mode_truth', u'pdg']
-
-Samples = [ "electron", "pion_0"]
 
 def shuffle_in_unison_inplace(a, b, c=False):
     assert len(a) == len(b)
@@ -16,7 +13,12 @@ def shuffle_in_unison_inplace(a, b, c=False):
         return a[p], b[p], c[p]
     return a[p], b[p]
 
-def DownSample(y,factor,Nx,Ny,Nz,Nw,sumabs=False):
+def DownSample(y,factor,batchsize,sumabs=False):
+    Nx=batchsize
+    Ny=y.shape[1]
+    Nz=y.shape[2]
+    Nw=y.shape[3]
+    
     if factor==0:
         return np.reshape(y,[Nx,Ny,Nz,Nw]),Nw
     # Remove entries at the end so Down Sampling works
@@ -24,113 +26,77 @@ def DownSample(y,factor,Nx,Ny,Nz,Nw,sumabs=False):
     features1=np.reshape(y,[Nx,Ny,Nz,Nw])[:,:,:,0:NwNew]
     # DownSample
     if sumabs:
-       features_Down=abs(features1.reshape([Nz*NwNew/factor,factor])).sum(axis=3).reshape([Nx,Ny,Nz,NwNew/factor])
+        features_Down=abs(features1.reshape([Nz*NwNew/factor,factor])).sum(axis=3).reshape([Nx,Ny,Nz,NwNew/factor])
     else:
         features_Down=features1.reshape([Nx,Ny,Nz*NwNew/factor,factor]).sum(axis=3).reshape([Nx,Ny,Nz,NwNew/factor])
     return features_Down, NwNew
 
-def DataGenerator(Samples,BatchSize=1024,DownSampleFactor=2,path="/data/LArIAT/h5_files"):
-    Files={}
-    NEvents={}
-    ClassIndex={}
+def GetXWindow(Data,i,BoxSizeX):
+    return Data[:,:,i:i+BoxSizeX]
+
+def ScanWindow(inData,BoxSizeX=256,Nx=240,Ny=4096):
+    NyNew=Ny
+    #Scan the Window
+
+    first=True
+    out=False
     
-    NSamples=len(Samples)
-    Status={}
+    for II in xrange(inData.shape[0]):
+        Data=inData[II]
+        b=np.array([0]*(NyNew-BoxSizeX))
+
+        for i in xrange(0,NyNew-BoxSizeX):
+            b[i]=GetXWindow(Data,i,BoxSizeX).clip(0,99999999999).sum()
+
+        #Find the Window with max Energy/Charge
+        BoxStart=b.argmax()
+        MaxSum=b[BoxStart]
+
+        #Store the window
+        if first:
+            first=False
+            Box=Data[:,:,BoxStart:BoxStart+BoxSizeX]
+            out=np.zeros((inData.shape[0],)+Box.shape)
+            out[II]=Box
+        else:
+            out[II]=Data[:,:,BoxStart:BoxStart+BoxSizeX]
+
+    return out,BoxStart,MaxSum
+     
+def FilterEnergy(MinEnergy):
+    def filterfunction(batchdict):
+        r= np.where(np.array(batchdict['Eng']) > MinEnergy)
+        return r[0]
+
+    return filterfunction
+
+def ProcessWireData(DownSampleFactor,ScanWindowSize,Norm=True):
+    def processfunction(D):
+        X=D[0]
+        BatchSize=X.shape[0]
+        if DownSampleFactor > 1:
+            X,Ny= DownSample(X,DownSampleFactor,BatchSize)
+        if ScanWindowSize>0:
+            X,i,j=ScanWindow(X,ScanWindowSize,240,Ny)
+
+        if Norm:
+            X = np.tanh(np.sign(X) * np.log(np.abs(X) + 1.0) / 2.0)
+        return [X]+D[1:]
+    return processfunction
     
-    for I,Sample in enumerate(Samples):
-        Files[Sample]=glob.glob(path+"/*"+Sample+"*.h5")
-        NEvents[Sample]=0
-        ClassIndex[Sample]=I
-        Status[Sample]= { "fileI":0,
-                          "eventI":0}
 
-    while True:
-        start=time()
-        First=True
-        I=0
-        NRead={}
-        for Sample in Files:
-            NRead[Sample]=BatchSize/NSamples
-        for Sample in Files:
-            NTotal=0
-            while NTotal<NRead[Sample]:
-                if BatchSize-I<NRead[Sample]:
-                    NRead[Sample]=BatchSize-I
-#                print Sample,Status
-         
-                FileName=Files[Sample][Status[Sample]["fileI"]]
-#                print "Opening File:",FileName
-                f=h5py.File(FileName,"r")
+def LArIATDataGenerator(FileSearch="/data/LArIAT/*.h5",DownSampleSize=4, ScanWindowSize=256,EnergyCut=0.61,
+                        datasetnames=[u'features'], Norm=False, MaxFiles=-1, **kwargs):
 
-                NEvents_File=f["features"].shape[0]
-
-                StartI=Status[Sample]["eventI"]
-
-#                print "NRead[Sample]", NRead[Sample]
-#                print "NEvents_File" , NEvents_File 
-#                print "StartI      " , StartI       
-
-                if NRead[Sample]<NEvents_File-StartI:
-                    NEnd=StartI+NRead[Sample]
-                    Status[Sample]["eventI"]+=NRead[Sample]                                        
-                else:
-                    NEnd=NEvents_File
-                    Status[Sample]["eventI"]=0
-                    Status[Sample]["fileI"]+=1
-
-                NTotal+=NEnd-StartI
-
-                InShape=f["features"].shape
-                
-                Nx = BatchSize
-                Ny = InShape[1]
-                Nz = InShape[2]
-                Nw = InShape[3]
-                
-                DataIn=f["features"][StartI:NEnd]
-                DataInDS,NwNew=DownSample(DataIn,DownSampleFactor,Nx,Ny,Nz,Nw)
-                
-                TheShape=(BatchSize,)+ DataInDS.shape[1:]
-
-                if First:
-                    Data_X=np.zeros(TheShape)
-                    Data_Y=np.zeros((BatchSize) )
-                    First=False
-                
-                Data_X[I:I+(NEnd-StartI)]=DataInDS
-                
-                a=np.empty(NEnd-StartI); a.fill(ClassIndex[Sample])
-#                print a.shape
-#                print Data_Y.shape
-#                print I
-                Data_Y[I:I+(NEnd-StartI)]=a
-                I+=NEnd-StartI
-                f.close()
-
-        Data_X,Data_Y=shuffle_in_unison_inplace(Data_X,Data_Y)
-        print "t=",time()-start, 
-                            
-        yield (Data_X,Data_Y),ClassIndex
-
-
-#xx=DataGenerator(Samples,BatchSize=1024)
-#
-#I=0
-#for (x,y),ClassIndex in xx:
-#    print x.shape,y.shape,ClassIndex
-#    if I>5:
-#        break
-#    I+=1
-
-
-def LArIATDataGenerator(datasetnames=[u'features'],batchsize=2048,FileSearch="/data/LArIAT/*.h5",MaxFiles=-1,
-                        verbose=False, OneHot=True, ClassIndex=False, ClassIndexMap=False,n_threads=4,
-                        multiplier=1,timing=False,**kwargs):
     print "Searching in :",FileSearch
     Files = glob.glob(FileSearch)
 
-    #if MaxFiles!=-1:
-    #    random.shuffle(Files)
+    print "Found",len(Files),"files."
+    
+    if MaxFiles!=-1:
+        random.shuffle(Files)
+        Files=Files[:MaxFiles]
+        
     Samples=[]
 
     FileCount=0
@@ -144,21 +110,12 @@ def LArIATDataGenerator(datasetnames=[u'features'],batchsize=2048,FileSearch="/d
         if MaxFiles>0:
             if FileCount>MaxFiles:
                 break
-
     
-    GC= DLMultiClassGenerator(Samples,batchsize,
-                              verbose=verbose, 
-                              #OneHot=OneHot,
-                              ClassIndex=ClassIndex,
-                              n_threads=n_threads,
-                              multiplier=multiplier,
-                              timing=timing, **kwargs)
-
-            
-    if ClassIndexMap:
-        return [GC,GC.ClassIndexMap]
-    else:
-        return GC
+    GC= DLMultiClassFilterGenerator(Samples, FilterEnergy(EnergyCut), 
+                                    preprocessfunction=ProcessWireData(DownSampleSize,ScanWindowSize,Norm),
+                                    **kwargs)
+    GC.datasets=datasetnames
+    return GC
 
 if __name__ == '__main__':
     import sys
@@ -167,25 +124,67 @@ if __name__ == '__main__':
     try:
         n_threads=int(sys.argv[1])
     except:
-        n_threads=4
+        n_threads=20
 
-    [Train_gen, CM]=LArIATDataGenerator(FileSearch=FileSearch,batchsize=128,n_threads=n_threads,ClassIndexMap=True)#,verbose=True)
+    try:
+        n_threads2=int(sys.argv[2])
+    except:
+        n_threads2=n_threads
 
-    print CM
+    closefiles=False
+    if n_threads>=61:
+        closefiles=True
+        
+    print "Building Generator"
+    sys.stdout.flush()
+    m=1
+    DownSampleSize=8
+    ScanWindowSize=256
+    Normalize=True
+    closefiles=False
+    Train_gen=LArIATDataGenerator(FileSearch=FileSearch,
+                                  cachefile="LArIAT-LoadDataTest-Cache.h5",
+                                  max=128*10000, 
+                                  batchsize=128,
+                                  DownSampleSize=DownSampleSize,
+                                  ScanWindowSize=ScanWindowSize,
+                                  Norm=Normalize,
+                                  #shapes=[(128*m, 2, 240, 4096/DownSampleSize), (128*m, 16)],
+                                  shapes=[(128*m, 2, 240, ScanWindowSize), (128*m, 16)],
+                                  n_threads=n_threads,
+                                  SharedDataQueueSize=1,
+                                  multiplier=m,
+                                  closefiles=closefiles,
+                                  verbose=False,
+                                  timing=False,
+                                  sleep=1,
+                                  Wrap=False)
+
+    print "Generator Ready"
+    print "ClassIndex:", Train_gen.ClassIndexMap
+    print "Object Shape:",Train_gen.shapes
 
     N=1
+    NN=n_threads
     count=0
-    start=time.time()
+    old=start=time.time()
     for tries in xrange(2):
         print "*********************Try:",tries
-        for D in Train_gen.Generator():
-            Delta=(time.time()-start)
-            print count,":",Delta, ":",Delta/float(N)
+        #for D in Train_gen.Generator():
+        for D in Train_gen.DiskCacheGenerator(n_threads2):
+            NN-=0
+            if NN<0:
+                break
+            start1=time.time()
+            Delta=(start1-start)
+            Delta2=(start1-old)
+            old=start1
+            print count,":",Delta, ":",Delta/float(N), Delta2
             sys.stdout.flush()
             N+=1
             for d in D:
                 print d.shape
-                print d[np.where(d!=0.)]
+                #print d[np.where(d!=0.)]
                 NN=d.shape[0]
                 #print d[0]
                 pass
